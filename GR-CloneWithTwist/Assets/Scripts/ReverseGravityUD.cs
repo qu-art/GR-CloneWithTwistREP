65acd0c4ad247b093b3ef13a99b302f7e9cad1a3
qu-art <196070830+qu-art@users.noreply.github.com>
2026-04-30 10:21:46 -0500

    Scripts add
    scripts added

---
+++ GR-CloneWithTwist/Assets/Scripts/ReverseGravityUD.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ReverseGravityUD : MonoBehaviour
+{
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.gravity.y = -Physics.gravity.y;
+
+    }
+}
