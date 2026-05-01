ec2b339f7abe1e8a410de3462cf1caff9682d7f2
qu-art <196070830+qu-art@users.noreply.github.com>
2026-05-01 12:33:24 -0500

    Gravity half fucntion
    grav

--- GR-CloneWithTwist/Assets/Scripts/PlayerController.cs
+++ GR-CloneWithTwist/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerTrigger"))
+        {
+            Debug.Log("Enetered trigger");
+        }
+    }
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- GR-CloneWithTwist/Assets/Scripts/ReverseGravityUD.cs
+++ GR-CloneWithTwist/Assets/Scripts/ReverseGravityUD.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class ReverseGravityUD : MonoBehaviour
 {
-    
+    public Collider coll;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody)
+            other.attachedRigidbody.useGravity = false;
     }
 
     // Update is called once per frame
